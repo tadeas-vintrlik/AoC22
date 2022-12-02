3393c199c9f5df3ddf9e4ddb1534d4b1f1eb742b
Tadeáš Vintrlík <Vintrlik@spravazeleznic.cz>
2022-12-02 13:53:26 +0100

    day2

---
+++ internal/day2/part1.go
@@ -0,0 +1,110 @@
+package day2
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+// Reponse of you/oponent Rock/paper/scissors
+type resp byte
+
+// o prefix means oponent
+var (
+	rock      resp = 'X'
+	paper     resp = 'Y'
+	scissors  resp = 'Z'
+	orock     resp = 'A'
+	opaper    resp = 'B'
+	oscissors resp = 'C'
+)
+
+type outcome int
+
+var (
+	defeat  outcome = 0
+	tie     outcome = 1
+	victory outcome = 2
+)
+
+// Score depedening on outcome (used as index)
+var score = []int{0, 3, 6}
+
+func (r resp) unexpected() string {
+	return fmt.Sprintf("invalid: %c", r)
+}
+
+// Expects a valid resp. Return score for resp type.
+func (r resp) getScore() int {
+	switch r {
+	case 'X':
+		return 1
+	case 'Y':
+		return 2
+	case 'Z':
+		return 3
+	default:
+		panic(r.unexpected())
+	}
+}
+
+// If r beats o.
+func (r resp) beats(o resp) bool {
+	return (o == orock && r == paper) || (o == opaper && r == scissors) || (o == oscissors && r == rock)
+}
+
+// If r ties with o.
+func (r resp) ties(o resp) bool {
+	return (o == orock && r == rock) || (o == opaper && r == paper) || (o == oscissors && r == scissors)
+}
+
+// Structure containing one line of input (response of you and your oponent)
+type roundStrat struct {
+	oponent resp
+	you     resp
+}
+
+// Get score for the outcome of the round.
+func (rs roundStrat) getRoundOutcome() outcome {
+	if rs.you.ties(rs.oponent) {
+		return tie
+	}
+	if rs.you.beats(rs.oponent) {
+		return victory
+	}
+	return defeat
+}
+
+// Get score for both outcome and your response.
+func (rs roundStrat) getTotalScore() int {
+	return score[rs.getRoundOutcome()] + rs.you.getScore()
+}
+
+// Create roundStart from line of input.
+func parseRoundStrat(line string) (roundStrat, error) {
+	s := strings.Split(line, " ")
+	o := s[0][0]
+	y := s[1][0]
+	you := resp(y)
+	them := resp(o)
+	return roundStrat{you: you, oponent: them}, nil
+}
+
+func Part1Solver(in string) int {
+	r := 0
+	for _, v := range strings.Split(in, "\n") {
+		rs, err := parseRoundStrat(v)
+		if err != nil {
+			panic(err)
+		}
+		r += rs.getTotalScore()
+	}
+	return r
+}
+
+func Part1() string {
+	return fmt.Sprintf("Part 1: %d", Part1Solver(input))
+}
---
+++ internal/day2/part2.go
@@ -0,0 +1,58 @@
+package day2
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+)
+
+// Get my response depending on o (oponent) response and the desired outcome.
+func (o resp) getMyResp(oc outcome) resp {
+	switch o {
+	case orock:
+		return [3]resp{scissors, rock, paper}[oc]
+	case opaper:
+		return [3]resp{rock, paper, scissors}[oc]
+	case oscissors:
+		return [3]resp{paper, scissors, rock}[oc]
+	default:
+		panic(o.unexpected())
+	}
+}
+
+// Part2 has new meaning for these characters these are infact desired outcomes.
+func (o resp) getRoundOutcomePart2() outcome {
+	switch o {
+	case 'X':
+		return defeat
+	case 'Y':
+		return tie
+	case 'Z':
+		return victory
+	default:
+		panic(o.unexpected())
+	}
+}
+
+// Get score for both the round outcome and our response.
+func (rs roundStrat) getTotalScorePart2() int {
+	oc := rs.you.getRoundOutcomePart2()
+	r := rs.oponent.getMyResp(oc)
+	return r.getScore() + score[oc]
+}
+
+func Part2Solver(in string) int {
+	r := 0
+	for _, v := range strings.Split(in, "\n") {
+		rs, err := parseRoundStrat(v)
+		if err != nil {
+			panic(err)
+		}
+		r += rs.getTotalScorePart2()
+	}
+	return r
+}
+
+func Part2() string {
+	return fmt.Sprintf("Part 2: %d", Part2Solver(input))
+}
