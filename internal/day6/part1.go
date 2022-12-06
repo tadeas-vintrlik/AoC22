e3f08f361848a568d53240af4d48516b335de031
Tadeáš Vintrlík <Vintrlik@spravazeleznic.cz>
2022-12-06 10:21:39 +0100

    day6 better

--- internal/day6/part1.go
+++ internal/day6/part1.go
@@ -2,6 +2,8 @@ package day6
 
 import (
 	_ "embed"
+	"fmt"
+	"strings"
 )
 
 //go:embed input.txt
@@ -10,21 +12,27 @@ var input string
 func unique(a string) bool {
 	max := len(a)
 	for i := 0; i < max; i++ {
-		for j := i + 1; j < len(a); j++ {
-			if a[i] == a[j] {
-				return false
-			}
+		if strings.IndexByte(a[i+1:], a[i]) != -1 {
+			return false
 		}
 	}
 	return true
 }
 
-func Part1Solver(in string) int {
+func bothPartsSolver(in string, size int) int {
 	max := len(in)
-	for i := 0; i+4 < max; i++ {
-		if unique(in[i : i+4]) {
-			return i + 4
+	for i := 0; i+size < max; i++ {
+		if unique(in[i : i+size]) {
+			return i + size
 		}
 	}
 	panic("solution not found")
+}
+
+func Part1Solver(in string) int {
+	return bothPartsSolver(in, 4)
+}
+
+func Part1() string {
+	return fmt.Sprintf("Part 2: %d", Part1Solver(input))
 }
